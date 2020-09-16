7680665b94694d02b268201c59c32f29d1c5d7f7
Hacker Man <electriccobras@gmail.com>
2020-09-15 20:17:14 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# autocppfiles README
+
+This extension automatically creates a .cpp and a .h file, with #pragma once as the first line of the .h file, with both named based on your input.
+
+## Features
+
+Right click on a file in the explorer and choose "Create C++ Files" to create the files in the same directory, or do the same to a folder to put the files in that folder. Using the command outside of this context menu will default to the same directory as the active file or the root directory of the workspace.
+
+## Requirements
+
+None.
+
+## Known Issues
+
+None so far. Please let me know if you run into any.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release
+
+-----------------------------------------------------------------------------------------------------------
---
+++ src/extension.ts
@@ -0,0 +1,47 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+
+function log(input: string) {
+	vscode.window.showInformationMessage(input);
+}
+
+function getWorkingPathDir(context: any, activeTextEditor: vscode.TextEditor | undefined, activeWorkspace: typeof vscode.workspace) {
+	if (context instanceof vscode.Uri) {
+		const fsPath = context.fsPath;
+		const stats = fs.statSync(fsPath);
+		return stats.isDirectory() ? fsPath : path.dirname(fsPath);
+	} if (activeTextEditor) {
+	  	return path.dirname(activeTextEditor.document.fileName);
+	} else {
+		return activeWorkspace.rootPath;
+	}
+}
+
+async function createNew(context: any) {
+	try {
+		const pathdir = getWorkingPathDir(context, vscode.window.activeTextEditor, vscode.workspace);
+
+		let fileNames: string | undefined = await vscode.window.showInputBox({prompt: "Input file name"});
+		
+		if (fileNames == undefined) { fileNames = ""; }
+
+		fs.writeFileSync(pathdir + "/" + fileNames + ".h", "#pragma once");
+		fs.writeFileSync(pathdir + "/" + fileNames + ".cpp", "");
+	} catch (err) {
+		log(err.message);
+	}
+}
+
+export function activate(context: any) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand("autocppfiles.createCppFiles", (context) =>
+			createNew(context)
+		)
+	);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
